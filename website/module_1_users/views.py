21513ca16d25e96d99cef976bd56c1dee56bc77d
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2024-02-04 20:30:08 +0200

    add pages on site

--- website/module_1_users/views.py
+++ website/module_1_users/views.py
@@ -10,3 +10,24 @@ from django.template import loader
 def index(request):
     # template = loader.get_template('index.html')
     return render(request, 'index.html') 
+
+def story(request):
+    return render(request, "story.html")
+
+def bot_translater(request):
+    return render(request, "translater.html")
+
+def examples(request):
+    return render(request, "examples.html")
+
+def my_project(request):
+    return render(request, "my_project.html")
+
+def ukrainian_story(request):
+    return render(request, "ukrainian_story.html")
+
+def traveling_story(request):
+    return render(request, "traveling_story.html")
+
+def contacts(request):
+    return render(request, "contacts.html")
