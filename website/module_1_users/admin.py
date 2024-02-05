64984d4acaecbcafc6df49a25dd5ec89dc262bac
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2024-02-05 10:40:11 +0200

    add models

--- website/module_1_users/admin.py
+++ website/module_1_users/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import UserProfile, Category, Story
+from .models import UserProfile, Category, Story, Project, Example, Traveling_Story, Ukrainian_Story
 
 admin.site.register(UserProfile)
 admin.site.register(Category)
 admin.site.register(Story)
+admin.site.register(Project)
+admin.site.register(Example)
+admin.site.register(Traveling_Story)
+admin.site.register(Ukrainian_Story)
--- website/module_1_users/models.py
+++ website/module_1_users/models.py
@@ -22,4 +22,18 @@ class Category(models.Model):
     name = models.CharField(max_length = 100)
     desctription = models.CharField(max_length = 1000)
 
-    
+class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length = 100)
+    desctription = models.CharField(max_length = 1000)
+    link = models.CharField(max_length = 100)
+    link_dithub = models.CharField(max_length = 100)
+
+class Example(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+class Traveling_Story(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+class Ukrainian_Story(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
