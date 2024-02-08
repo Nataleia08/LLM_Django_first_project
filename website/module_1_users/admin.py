cbdf65f0e1e96a109201fbf0a8aa334fdcf238fa
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2024-02-08 11:55:50 +0200

    model draft 2

--- website/module_1_users/admin.py
+++ website/module_1_users/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-from .models import UserProfile, Category, Story, Project, Example, Traveling_Story, Ukrainian_Story
+from .models import UserProfile, Category, Story, Project, Example, Tag
 
 admin.site.register(UserProfile)
-admin.site.register(Category)
 admin.site.register(Story)
 admin.site.register(Project)
 admin.site.register(Example)
-admin.site.register(Traveling_Story)
-admin.site.register(Ukrainian_Story)
+admin.site.register(Tag)
--- website/module_1_users/models.py
+++ website/module_1_users/models.py
@@ -1,26 +1,46 @@
 from django.db import models
 import uuid
+from django_enum import EnumField
+
+
+class TypeStory(models.TextChoices):
+    BASIC = "B", "basic"
+    TRAVELING = "T", "traveling"
+    UKRAINIAN = "U", "ukrainian"
 
 # Create your models here.
-
-
 class UserProfile(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length = 100)
     email = models.EmailField(max_length = 200)
-    
+
+
+class Tag(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=25, null=False, unique=True)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+
+class Category(models.TextChoices):
+    name = models.CharField(max_length = 100, unique = True)
+
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Story(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user_id = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     text = models.TextField()
     title = models.CharField(max_length = 100)
+    type = EnumField(TypeStory)
+    tags = models.ManyToManyField(Tag)
+    category = EnumField(Category)
 
-
-class Category(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length = 100)
-    desctription = models.CharField(max_length = 1000)
 
 class Project(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -28,12 +48,15 @@ class Project(models.Model):
     desctription = models.CharField(max_length = 1000)
     link = models.CharField(max_length = 100)
     link_dithub = models.CharField(max_length = 100)
+    tags = models.ManyToManyField(Tag)
+    category = EnumField(Category)
+
 
 class Example(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    tags = models.ManyToManyField(Tag)
+    category = EnumField(Category)
+    dataset = models.TextField()
+    result = models.TextField()
 
-class Traveling_Story(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
-class Ukrainian_Story(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
