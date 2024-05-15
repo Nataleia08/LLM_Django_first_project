3891803eb5dae3618a2589d35460d0f2f3cc826f
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2024-05-15 11:19:02 +0300

    tags

--- story_site/story/templatetags/extract_cat.py
+++ story_site/story/templatetags/extract_cat.py
@@ -2,10 +2,8 @@ from django import template
 
 register = template.Library()
 
-
-def categories(story_categories):
+def serchcat(story_categories):
     return ', '.join([str(name) for name in story_categories.all()])
 
-
-register.filter('categories', categories)
+register.filter('serchcat', serchcat)
 
